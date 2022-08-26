3c11574033918e8e55316f401407c0c4124fca99
Iris Saladino <iris.r.saladino@gmail.com>
2022-08-25 23:53:05 -0300

    feat: Adjust port in bridge; add "bridge" script on package.json

--- bridge.js
+++ bridge.js
@@ -1,20 +1,19 @@
-const OSC = require('osc-js')
+import OSC from 'osc-js'
 
 const config = {
-    wsServer: { port: 8080 },
-    udpClient: { port: 9129 },
-    udpServer: { port: 9130 },
+  wsServer: { port: 8080 },
+  udpClient: { port: 9129 },
+  udpServer: { port: 9130 },
 }
 const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
 
 osc.on('/rms', message => {
-    console.log("RMS", message.args)
+  console.log("RMS", message.args)
 })
 
 osc.on('open', () => {
-    console.log("Port is open now.")
-    const message = new OSC.Message('/test', 12.221, 'hello')
-    osc.send(message)
+  console.log(`Port ${config.wsServer.port} is open now.`)
+  console.log(`Will redirect RMS messages to UDP port ${config.udpClient.port}`)
 })
 
 osc.open()
